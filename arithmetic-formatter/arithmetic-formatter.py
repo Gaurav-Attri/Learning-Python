931c8dd1a36f0ad13e67946a04cc30e060a9242f
Gaurav-Attri <attrigaurav60@gmail.com>
2025-11-15 17:32:53 +0530

    Created arithmetic-formatter

--- arithmetic-formatter/arithmetic-formatter.py
+++ arithmetic-formatter/arithmetic-formatter.py
@@ -1,20 +1,24 @@
+def get_operands_and_operator(problem):
+    additionOperator = True if problem.find('+') != -1 else False
+    operands = problem.split(' + ' if additionOperator else ' - ')
+    return operands, additionOperator
+
 def contains_digits_only(problems):
     for problem in problems:
-        additionOperator = True if problem.find('+') != -1 else False
-        operands = problem.split(' + ' if additionOperator else ' - ')
+        operands = get_operands_and_operator(problem)[0]
         if not (operands[0].isdigit() and operands[1].isdigit()):
             return False
     return True
 
 def operands_have_four_digits_only(problems):
     for problem in problems:
-        additionOperator = True if problem.find('+') != -1 else False
-        operands = problem.split(' + ' if additionOperator else ' - ')
+        operands = get_operands_and_operator(problem)[0]
         if not (len(operands[0]) <= 4 and len(operands[1]) <= 4):
             return False
     return True
 
 def arithmetic_arranger(problems, show_answers=False):
+    # Wrong Format input handling code
     if len(problems) > 5:
         return 'Error: Too many problems.'
     if len(list(filter(lambda problem: problem.find('*') != -1 or problem.find('/') != -1, problems))) != 0:
@@ -24,4 +28,52 @@ def arithmetic_arranger(problems, show_answers=False):
     if not operands_have_four_digits_only(problems):
         return 'Error: Numbers cannot be more than four digits.'
 
-print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
+    # Correct Format input handling code
+    first_output_line = ''
+    second_output_line = ''
+    third_output_line = ''
+    fourth_output_line = ''
+    final_output = ''
+
+    for problem in problems:
+        string_operands, additionOperator = get_operands_and_operator(problem)
+        operands = list(map(lambda operand: int(operand), string_operands))
+        result = operands[0] + operands[1] if additionOperator else operands[0] - operands[1]
+
+        left_operand_smallest = True if len(string_operands[0]) < len(string_operands[1]) else False
+        operands_len_diff = abs(len(string_operands[0]) - len(string_operands[1]))
+        max_len_operand = max(len(string_operands[0]), len(string_operands[1]))
+
+        string_operands[0] = (string_operands[0][::-1] + ' '*2)[::-1]
+        # string_operands[1] = ('+ ' if additionOperator else '- ') + string_operands[1]
+
+        if left_operand_smallest:
+            string_operands[0] = (string_operands[0][::-1] + ' '*operands_len_diff)[::-1]
+        else:
+            string_operands[1] = (string_operands[1][::-1] + ' '*operands_len_diff)[::-1]
+        
+        string_operands[1] = (string_operands[1][::-1] + (' +' if additionOperator else ' -'))[::-1]
+
+
+        string_operands[0] = string_operands[0] + ' '*4
+        string_operands[1] = string_operands[1] + ' '*4
+        
+        first_output_line += string_operands[0]
+        second_output_line += string_operands[1]
+        third_output_line += '-'*(max_len_operand+2) + ' '*4
+        fourth_output_line += (str(result)[::-1] + ' '*((max_len_operand+2)-len(str(result))))[::-1] + ' '*4
+        
+    
+    final_output = first_output_line + '\n' + second_output_line + '\n' + third_output_line + '\n'
+
+    if show_answers:
+        final_output += fourth_output_line + '\n'
+
+    return final_output
+        
+
+
+
+print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True)}')
+
+print(f'\n{arithmetic_arranger(["32 - 698", "1 - 3801", "45 + 43", "123 + 49", "988 + 40"], True)}')
